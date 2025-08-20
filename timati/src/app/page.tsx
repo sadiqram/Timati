f6ef7d7dfeb3408280c67e7e4696ba030e22ec1f
sadiqram <abdulrahmansadiq13@gmail.com>
2025-08-20 16:24:31 -0500

    add timer component to page

--- timati/src/app/page.tsx
+++ timati/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Button from "@/components/Button";
+import Timer from  "@/components/Timer"
 
 
 
@@ -9,22 +10,25 @@ import Button from "@/components/Button";
 
 export default function Home() {
   return (
-
     <div>
-      <div className= " flex flex-col items-center justify-center h-screen">
-        <div className="border p-6 rounded-lg">
-        <div className=" justify-center text-2xl font-bold">Session Title</div>
-        
-        <p>23:00</p>
-        <div className="flex gap-2 cursor-pointer">
-        <Button>Start</Button>
-        <Button>Pause</Button>
-        <Button>Reset</Button>
-        </div>
-        <p>Session Type 1</p>
-
-        </div>
-      </div>
+      <Timer />
     </div>
+
+    // <div>
+    //   <div className= " flex flex-col items-center justify-center h-screen">
+    //     <div className="border p-6 rounded-lg">
+    //     <div className=" justify-center text-2xl font-bold">Session Title</div>
+        
+    //     <p>23:00</p>
+    //     <div className="flex gap-2 cursor-pointer">
+    //     <Button>Start</Button>
+    //     <Button>Pause</Button>
+    //     <Button>Reset</Button>
+    //     </div>
+    //     <p>Session Type 1</p>
+
+    //     </div>
+    //   </div>
+    // </div>
   );
 }

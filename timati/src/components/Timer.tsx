f15ea54ce12e3d1468e41566186d0e35ca3d9add
sadiqram <abdulrahmansadiq13@gmail.com>
2025-08-20 15:38:02 -0500

    Implement startTimer functionality

--- timati/src/components/Timer.tsx
+++ timati/src/components/Timer.tsx
@@ -1,62 +1,88 @@
-// "use client";
+"use client";
 
-// import { useState, useEffect, useCallback } from "react";
-// type SessionType = "pomodoro" | "shortBreak" | "longBreak" | "custom";
+import { time } from "console";
+import { useState, useEffect} from "react";
+type SessionType = "pomodoro" | "shortBreak" | "longBreak" | "custom";
 
-// interface TimerSettings{
-//     pomodoro: number;
-//     shortBreak: number;
-//     longBreak: number;
-//     custom: number;
-//     cycles: number;
-//     isRunning: boolean;
-//     isPaused: boolean;
-//     isFinished: boolean;
-// }
+interface TimerSettings{
+    pomodoro: number;
+    shortBreak: number;
+    longBreak: number;
+    custom: number;
+    cycles: number;
+    isRunning: boolean;
+    isPaused: boolean;
+    isFinished: boolean;
+}
 
-// export default function Timer() {
-//   const [timeleft, setTimeleft] = useState(25*60);
-//   const [sessionType, setSessionType] = useState<SessionType>("pomodoro");
-//   const [isRunning, setIsRunning] = useState(false);
-//   const [isPaused, setIsPaused] = useState(false);
-//   const [isFinished, setIsFinished] = useState(false);
-//   const [completedSessions, setCompletedSessions] = useState(0);
-//   const [cycles, setCycles] = useState(0);
-//   const [isCustomTime, setIsCustomTime] = useState(false);
+export default function Timer() {
+  const [timeleft, setTimeleft] = useState(25*60);
+  const [sessionType, setSessionType] = useState<SessionType>("pomodoro");
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+  const [completedSessions, setCompletedSessions] = useState(0);
+  const [cycles, setCycles] = useState(0);
+  const [isCustomTime, setIsCustomTime] = useState(false);
   
-//  const settings : TimerSettings = {
-//     pomodoro: 25*60,
-//     shortBreak: 5*60,
-//     longBreak: 15*60,
-//     custom: 0,
-//     cycles: 4,
-//     isRunning: false,
-//     isPaused: false,
-//     isFinished: false,
-//  }
+ const settings : TimerSettings = {
+    pomodoro: 25*60,
+    shortBreak: 5*60,
+    longBreak: 15*60,
+    custom: 0,
+    cycles: 4,
+    isRunning: false,
+    isPaused: false,
+    isFinished: false,
+ }
+ useEffect(() => {
+    let timer: NodeJS.Timeout;
+    
+    if (isRunning && !isPaused && timeleft > 0 && !isFinished) {
+        timer = setInterval(() => {
+            setTimeleft((prev) => prev - 1);
+        }, 1000);
+    } 
+    
+    if (timeleft === 0) {
+        setIsRunning(false);
+        setIsPaused(false);
+        setIsFinished(true);
+    }
 
-//  const getProgress = () => {
-//     const totalTime = settings.pomodoro + settings.shortBreak + settings.longBreak;
-//     const progress = (timeleft / totalTime) * 100;
-//     return progress;
-//  }
+    return () => clearInterval(timer)
+ }, [isRunning, isPaused,timeleft]);
+ const getProgress = () => {
+    const totalTime = settings.pomodoro + settings.shortBreak + settings.longBreak;
+    const progress = (timeleft / totalTime) * 100;
+    return progress;
+ }
 
-//  const startTimer = () => {
-//     setIsRunning(true);
-//     setIsPaused(false);
-//     setIsFinished(false);
-//  }
+ const startTimer = () => {
+    setIsRunning(true);
+    setIsPaused(false);
+    setIsFinished(false);
 
-//  const switchSession = () => {
+    // Reset to correct seesion length when starting afresh
+    if (sessionType === "custom") {
+        setTimeleft(settings.custom);
+    } else {
+        setTimeleft(settings[sessionType]);
+    }
 
-//  }
+    // Start the timer
+ }
 
-//  const pauseTimer = () => {}
+ const switchSession = () => {
 
-//  const resetTimer = () => {}
-//  const pauseTimer = () => {}
+ }
+
+ const pauseTimer = () => {}
+
+ const resetTimer = () => {}
+ const pauseTimer = () => {}
 
 
 
-//   return <div>Timer</div>;
-// }
+  return <div>Timer</div>;
+}

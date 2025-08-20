cbdd5fa3f130133690d14362cc4930b0742aba15
sadiqram <abdulrahmansadiq13@gmail.com>
2025-08-20 16:24:15 -0500

    update timer component with functionality for pause, start, reset

--- timati/src/components/Timer.tsx
+++ timati/src/components/Timer.tsx
@@ -2,6 +2,8 @@
 
 import { time } from "console";
 import { useState, useEffect} from "react";
+import Button from "./Button";
+import Magnetic from "./Magnetic";
 type SessionType = "pomodoro" | "shortBreak" | "longBreak" | "custom";
 
 interface TimerSettings{
@@ -52,6 +54,7 @@ export default function Timer() {
 
     return () => clearInterval(timer)
  }, [isRunning, isPaused,timeleft]);
+
  const getProgress = () => {
     const totalTime = settings.pomodoro + settings.shortBreak + settings.longBreak;
     const progress = (timeleft / totalTime) * 100;
@@ -70,19 +73,91 @@ export default function Timer() {
         setTimeleft(settings[sessionType]);
     }
 
-    // Start the timer
+ }
+
+ const formatTime = (time:number) => {
+    const minutes = Math.floor(time / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`; 
+ }
+
+ 
+
+ const pauseTimer = () => {
+    setIsPaused(true);
+ }
+ const resumeTimer =  () => {
+    setIsRunning(true)
+    setIsPaused(false)
+    setIsFinished(false)
+ }
+
+ const resetTimer = () => {
+    setIsRunning(false);
+    setIsPaused(false);
+    setIsFinished(false);
+    setTimeleft(settings.pomodoro);
  }
 
  const switchSession = () => {
+    if (sessionType === "pomodoro"){
+        // finished a focus session
+        setCompletedSessions((prev)=> prev + 1)
+        // Only after every 4 Pomodoros, take a long break, else take short break
+        if ((completedSessions + 1) % 4 === 0) {
+            setSessionType("longBreak")
+            setTimeleft(settings.longBreak)
+        }
+        else {
+            setSessionType("shortBreak")
+            setTimeleft(settings.shortBreak) 
+        }
+    }
+    else if (sessionType === "shortBreak" || sessionType === "longBreak"){
+        setSessionType("pomodoro")
+        setTimeleft(settings.pomodoro)
+    }
+    //Reset state
+    setIsFinished(false)
+    setIsPaused(false)
+    setIsRunning(false)
 
  }
 
- const pauseTimer = () => {}
+  return (
+  <div>
+    <div className= " flex flex-col items-center justify-center h-screen">
+        <div className="border p-6 rounded-lg text-center ">
+            {/* Title */}
+            <div className=" justify-center text-2xl font-bold">
+                {sessionType.toUpperCase()}
+            </div>
+            {/* Timer display */}
+            <p className="text-4xl font-mono mb-4">{formatTime(timeleft)}</p>
 
- const resetTimer = () => {}
- const pauseTimer = () => {}
+            {/* Controls */}
+            <div className="flex gap-2 cursor-pointer mb-4">
+                {!isRunning && !isPaused && (<Magnetic><button onClick = {startTimer}> Start </button></Magnetic>
+            )}
+
+            {isRunning && !isPaused && (<Magnetic><button onClick = {pauseTimer}> Pause </button></Magnetic>
+            )}
+            {( isPaused && !isFinished) && (<Magnetic><button onClick = {resumeTimer}> Resume </button></Magnetic>
+            )}
+                
+            <Magnetic><button onClick = {resetTimer}> Reset </button></Magnetic>
+            </div>
 
 
 
-  return <div>Timer</div>;
+
+        </div>
+
+    </div>
+
+  </div>
+
+);
 }

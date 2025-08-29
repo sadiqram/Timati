c3fe4e5448a29f1f019c0fc89194c314f2bf4fb8
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2025-08-28 21:55:03 -0400

    install supabase and begin initial setup

---
+++ timati/src/auth/supabase.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+
+
+const supabase = createClient(
+    import.meta.env.NEXT_PUBLIC_SUPABASE_URL,
+    import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
+
+//example sign in
+
+// const {data,error} = await supabase.auth.signInWithPassword({
+//     email: "test@example.com",
+//     password: "mypassword"
+// })
+export default supabase;

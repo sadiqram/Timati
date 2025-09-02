0ff7dcbb84678cc567d173146c2b325dfd1d15c4
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2025-09-02 15:49:10 -0400

    adjust db parameters

--- timati/src/auth/supabase.ts
+++ timati/src/auth/supabase.ts
@@ -1,13 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
+import { env } from "process";
 
 
 
 
 const supabase = createClient(
-    import.meta.env.NEXT_PUBLIC_SUPABASE_URL,
-    import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    // import.meta.env.NEXT_PUBLIC_SUPABASE_URL,
+    // import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
-
 //example sign in
 
 // const {data,error} = await supabase.auth.signInWithPassword({

41b0d21d49258082f596699c08331db1ba01730c
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2025-09-02 15:48:50 -0400

    adjust spotify embedd height/width

--- timati/src/components/SpotifyEmb.tsx
+++ timati/src/components/SpotifyEmb.tsx
@@ -5,7 +5,7 @@ export default function SpotifyEmb({playlistId}: {playlistId: string}){
         <iframe 
         src={`https://open.spotify.com/embed/playlist/${playlistId}`} 
         width = "300"
-        height = "380"
+        height = "180"
         frameBorder="0"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
